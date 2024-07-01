74bd5f8211f48ddb1dfdb50068e0b0958138088b
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2024-07-01 18:42:37 +0200

    fix stuff, now running properly

--- analyzer/dumperReader/reader.py
+++ analyzer/dumperReader/reader.py
@@ -12,10 +12,13 @@ from .tracksters import *
 from .recoToSim import *
 from .simToReco import *
 
+from typing import List
 
+import awkward as ak
+print(ak.__version__)
 
 def superclustersToEnergy(supercls_ts_idxs:ak.Array, tracksters:ak.Array) -> ak.Array:
-    """ Computes the total energy of a supercluster from an array of supercluster ids 
+    """ Computes the total energy of a supercluster from an array of supercluster ids
     Preserves the inner index (usually CP id)
     Parameters :
      - supercls_ts_idxs : type nevts * var * var * uint64
@@ -30,14 +33,14 @@ def superclustersToEnergy(supercls_ts_idxs:ak.Array, tracksters:ak.Array) -> ak.
 
 
 
-    
+
 class DumperReader:
     """ Reads TICLDumper output """
     class MultiFileReader:
         """ From a list of files opened with uproot, find the the first one which has the requested key (for finding the file having the ticlDumper among different files holding the same events) """
         def __init__(self, files:list[uproot.ReadOnlyDirectory]):
             self.files = files
-        
+
         def __getitem__(self, key):
             excpts = []
             for file in self.files:
@@ -46,9 +49,9 @@ class DumperReader:
                 except uproot.KeyInFileError as e:
                     excpts.append(e)
             raise KeyError(*excpts)
-            
-    def __init__(self, file:str|uproot.ReadOnlyDirectory|list[uproot.ReadOnlyDirectory], directoryName:str="ticlDumper") -> None:
-        """ file can be : 
+
+    def __init__(self, file: Union[str, uproot.ReadOnlyDirectory, List[uproot.ReadOnlyDirectory]], directoryName: str = "ticlDumper") -> None:
+        """ file can be :
          - a str : opens the file and loads ticlDumper
          - an already opened uproot file
          - a list of uproot files, in which case it opens them in order and uses the first one that has ticlDumper in it
@@ -60,22 +63,38 @@ class DumperReader:
                 self.fileDir = self.MultiFileReader([f[directoryName] for f in file])
             except TypeError:
                 self.fileDir = uproot.open(file + ":" + directoryName)
-    
+
     @property
     def nEvents(self):
-        return self.fileDir["trackstersCLUE3DHigh"].num_entries
+        return self.fileDir["tracksters"].num_entries
 
     @cached_property
     def tracksters(self) -> ak.Array:
-        return self.fileDir["trackstersCLUE3DHigh"].arrays()
-    
+        return self.fileDir["tracksters"].arrays()
+
     @cached_property
     def tracksters_zipped(self) -> ak.Array:
-        return ak.zip({"ts_id" : ak.local_index(self.tracksters.raw_energy, axis=1)} | 
-                      {key : self.tracksters[key] for key in self.tracksters.fields 
-                        if key not in ["event", "NClusters", "NTracksters"]},
+        return ak.zip({"ts_id" : ak.local_index(self.tracksters.raw_energy, axis=1)} |
+                      {key : self.tracksters[key] for key in self.tracksters.fields
+                        if key not in ["vertices_multiplicity","event", "NClusters", "NTracksters"]},
             depth_limit=2, # don't try to zip vertices
             with_name="tracksters"
+        )
+    @cached_property
+    def trackstersMerged(self) -> ak.Array:
+        return self.fileDir["trackstersMerged"].arrays()
+
+    @cached_property
+    def trackstersMerged_zipped(self) -> ak.Array:
+        # Create the base dictionary
+        base_dict = {"ts_id": ak.local_index(self.trackstersMerged.raw_energy, axis=1)}
+        # Update the base dictionary with the other fields
+        base_dict.update({key: self.trackstersMerged[key] for key in self.trackstersMerged.fields
+                          if key not in ["vertices_multiplicity", "event", "NClusters", "NTracksters"]})
+        return ak.zip(
+            base_dict,
+            depth_limit=2,  # don't try to zip vertices
+            with_name="trackstersMerged"
         )
 
     @cached_property
@@ -84,14 +103,14 @@ class DumperReader:
     @cached_property
     def simTrackstersCP_df(self) -> pd.DataFrame:
         return ak.to_dataframe(self.simTrackstersCP, levelname=lambda x : {0:"eventInternal", 1:"caloparticle_id"}[x])
-    
+
     @cached_property
     def superclusters(self) -> ak.Array:
         """ Gets the supercluster trackster ids (since ticlv5 this actually includes also tracksters in one-trackster superclusters)
         type: nevts * var (superclsCount) * var (trackstersInSupercls) * uint64 (trackster id)
         """
         return self.fileDir["superclustering/linkedResultTracksters"].array()
-    
+
     @cached_property
     def superclusters_all(self) -> ak.Array:
         """ Supercluster tracksters ids. Tracksters not in a supercluster are included in a one-trackster supercluster each
@@ -107,9 +126,9 @@ class DumperReader:
 
     @cached_property
     def associations(self) -> ak.Array:
-        return self.fileDir["associations"].arrays(filter_name=["event_", "tsCLUE3D_*"])
-    
-    
+        return self.fileDir["associations"].arrays(filter_name=["event_", "tsCLUE3D_*", 'Mergetracksters_*', 'Mergetstracksters_*'])
+
+
     @cached_property
     def supercluster_df(self) -> pd.DataFrame:
         return ak.to_dataframe(self.superclusters, anonymous="ts_id",
@@ -122,11 +141,11 @@ class DumperReader:
         return self.supercluster_df
         # return ak.to_dataframe(self.superclusters_all, anonymous="ts_id",
         #     levelname=lambda x : {0:"eventInternal", 1:"supercls_id", 2:"ts_in_supercls_id"}[x])
-    
+
     @cached_property
     def supercluster_merged_properties_all(self) -> pd.DataFrame:
-        """ Dataframe holding supercluster properties (one row per supercluster) 
-        
+        """ Dataframe holding supercluster properties (one row per supercluster)
+
         Tracksters not in a supercluster are included as one-trackster superclusters
         """
         return (supercluster_joinTracksters(self.supercluster_all_df, self.tracksters_zipped[["raw_energy", "regressed_energy"]])
@@ -135,42 +154,96 @@ class DumperReader:
                     raw_energy_supercls=pd.NamedAgg("raw_energy", "sum"),
                     regressed_energy_supercls=pd.NamedAgg("regressed_energy", "sum"),
         ))
-    
+
     @cached_property
     def assocs_bestScore_simToReco_df(self) -> pd.DataFrame:
-        """ Make a Df of largest score associations of each SimTrackster 
-        
+        """ Make a Df of largest score associations of each SimTrackster
+
         Index eventInternal	ts_id, column : caloparticle_id
         """
         # Get largest association score
         assocs_simToReco_largestScore = assocs_bestScore(assocs_zip_simToReco(self.associations))
         # Make a df out of it : index eventInternal	ts_id, column : caloparticle_id
-        return  (ak.to_dataframe(assocs_simToReco_largestScore[["ts_id", "caloparticle_id", "score", "sharedE"]], 
+        return  (ak.to_dataframe(assocs_simToReco_largestScore[["ts_id", "caloparticle_id", "score", "sharedE"]],
                                 levelname=lambda x : {0:"eventInternal", 1:"caloparticle_id_wrong"}[x])
                 .reset_index("caloparticle_id_wrong", drop=True)
                 .set_index("caloparticle_id", append=True)
         )
-    
+
     def assocs_bestScore_recoToSim_df(self, dropOnes=True) -> pd.DataFrame:
-        """ Make a Df of largest score associations of each Trackster 
-        
-        Parameters : 
+        """ Make a Df of largest score associations of each Trackster
+
+        Parameters :
          - dropOnes : if True, do not include associations of score 1 (worst score)
         Index eventInternal	caloparticle_id, column : ts_id, score, sharedE
         """
         # Get largest association score
         assocs = assocs_bestScore((assocs_dropOnes if dropOnes else lambda x:x)(assocs_zip_recoToSim(self.associations)))
         # Make a df out of it : index eventInternal	ts_id, column : caloparticle_id
-        return (ak.to_dataframe(assocs[["ts_id", "caloparticle_id", "score", "sharedE"]], 
+        return (ak.to_dataframe(assocs[["ts_id", "caloparticle_id", "score", "sharedE"]],
                                 levelname=lambda x : {0:"eventInternal", 1:"ts_id_wrong"}[x])
                 .reset_index("ts_id_wrong", drop=True)
                 .set_index("ts_id", append=True)
         )
-        
-    
+
+    @cached_property
+    def assocs_bestScore_simToRecoMerged_df(self) -> pd.DataFrame:
+        """ Make a Df of largest score associations of each SimTrackster
+
+        Index eventInternal	ts_id, column : caloparticle_id
+        """
+        # Get largest association score
+        assocs_simToReco_largestScore = assocs_bestScore(assocs_zip_simToRecoMerged(self.associations))
+        # Make a df out of it : index eventInternal	ts_id, column : caloparticle_id
+        return  (ak.to_dataframe(assocs_simToReco_largestScore[["ts_id", "caloparticle_id", "score", "sharedE"]],
+                                levelname=lambda x : {0:"eventInternal", 1:"caloparticle_id_wrong"}[x])
+                .reset_index("caloparticle_id_wrong", drop=True)
+                .set_index("caloparticle_id", append=True)
+        )
+
+    @cached_property
+    def assocs_bestScore_simToRecoShared_df(self) -> pd.DataFrame:
+        """Make a DataFrame of associations of each SimTrackster with shared energy > 0.
+
+        Index: eventInternal, ts_id
+        Columns: caloparticle_id
+        """
+        # Get associations where sharedE > 0
+        assocs_simToReco_sharedE = assocs_zip_simToReco(self.associations)
+
+        assocs_simToReco_sharedE = assocs_simToReco_sharedE[assocs_simToReco_sharedE["sharedE"] > 0]
+
+        # Make a DataFrame out of it: index eventInternal, ts_id; columns: caloparticle_id
+        print(assocs_simToReco_sharedE)
+        def levelname(x):
+            names = ["eventInternal", "caloparticle_id_wrong"] + [f"level_{i}" for i in range(x - 2)]
+            return names[x] if x < len(names) else f"level_{x}"
+        return (
+            ak.to_dataframe(assocs_simToReco_sharedE[["ts_id", "caloparticle_id", "score", "sharedE"]],
+                            levelname=levelname)
+            .reset_index("caloparticle_id_wrong", drop=True)
+            .set_index("caloparticle_id", append=True)
+        )
+
+
+    def assocs_bestScore_recoMergedToSim_df(self, dropOnes=True) -> pd.DataFrame:
+        """ Make a Df of largest score associations of each Trackster
+
+        Parameters :
+         - dropOnes : if True, do not include associations of score 1 (worst score)
+        Index eventInternal	caloparticle_id, column : ts_id, score, sharedE
+        """
+        # Get largest association score
+        assocs = assocs_bestScore((assocs_dropOnes if dropOnes else lambda x:x)(assocs_zip_recoMergedToSim(self.associations)))
+        # Make a df out of it : index eventInternal	ts_id, column : caloparticle_id
+        return (ak.to_dataframe(assocs[["ts_id", "caloparticle_id", "score", "sharedE"]],
+                                levelname=lambda x : {0:"eventInternal", 1:"ts_id_wrong"}[x])
+                .reset_index("ts_id_wrong", drop=True)
+                .set_index("ts_id", append=True)
+        )
 class Step3Reader:
     """ Reads step3 split EDM files """
-    def __init__(self, file:str|uproot.ReadOnlyDirectory) -> None:
+    def __init__(self, file: Union[str, uproot.ReadOnlyDirectory]) -> None:
         try:
             self.eventsTree = file["Events"]
             self.file = file
@@ -186,12 +259,13 @@ class FWLiteDataframesReader:
         self.sampleNb = sampleNb
         self.eventMapping = ak.to_dataframe(readerForEventMapping.tracksters.event_, levelname=lambda x:"eventInternal", anonymous="event_").reset_index()
         """ Dataframe mapping eventInternal (the index into the TICLDumper file) and the actual CMS event number (event_) """
-    
+
     def readDataframe(self, name:str) -> pd.DataFrame:
         return pd.read_pickle(Path(self.folder) / f"{name}_{self.sampleNb}.pkl.gz")
-    
+
     @cached_property
     def supercls(self) -> pd.DataFrame:
         return pd.merge(self.readDataframe("supercls"), self.eventMapping, on="event_").set_index(["eventInternal", "supercls_id", "ts_in_supercls_id"])
 
 #ticlTracksters_ticlTracksterLinksSuperclustering_CLUE3D_RECO./ticlTracksters_ticlTracksterLinksSuperclustering_CLUE3D_RECO.obj/ticlTracksters_ticlTracksterLinksSuperclustering_CLUE3D_RECO.obj.regressed_energy_
+
--- analyzer/energy_resolution/fit.py
+++ analyzer/energy_resolution/fit.py
@@ -7,6 +7,9 @@ from scipy.optimize import curve_fit
 import numpy as np
 import hist
 import pandas as pd
+import matplotlib.pyplot as plt
+import mplhep as hep
+plt.style.use(hep.style.CMS)
 
 @dataclass
 class CruijffParam:
@@ -21,12 +24,13 @@ class CruijffParam:
     alphaL:float
     """ Left tail alpha """
     alphaR:float
-    """ Right tail alpha """
+#     sigmaEff:float
+
 
     @property
     def sigmaAverage(self) -> float:
         return (self.sigmaL + self.sigmaR) / 2
-    
+
     def makeTuple(self) -> tuple[float]:
         return dataclasses.astuple(self)
 
@@ -43,6 +47,8 @@ def cruijff(x, A, m, sigmaL,sigmaR, alphaL, alphaR):
     AR = np.full(x.shape, alphaR)
     sigma = np.where(dx<0, SL,SR)
     alpha = np.where(dx<0, AL,AR)
+    if(sigmaL == sigmaR):
+        sigma = sigmaL
     f = 2*sigma*sigma + alpha*dx*dx
     return A* np.exp(-dx*dx/f)
 
@@ -52,7 +58,7 @@ def histogram_quantiles(h:hist.Hist, quantiles):
     """
     # adapated from https://stackoverflow.com/a/61343915
     assert len(h.axes) == 1 and h.storage_type == hist.storage.Double, "Histogram quantiles needs a 1D double (non-weighted) histogram"
-    cdf = (np.cumsum(h.values()) - 0.5 * h.values()) / np.sum(h.values()) 
+    cdf = (np.cumsum(h.values()) - 0.5 * h.values()) / np.sum(h.values())
     return np.interp(quantiles, cdf, h.axes[0].centers)
 
 def fitCruijff(h_forFit:hist.Hist) -> CruijffFitResult:
@@ -68,15 +74,15 @@ def fitCruijff(h_forFit:hist.Hist) -> CruijffFitResult:
         median-q_min1, #sigmaL : this quantile difference is 1sigma for a Gaussian
         q_plus1-median, # sigmaR
         (q_min1-q_min2) / (median-q_min1)/3.81 * 0.28067382, #alphaL : in the ratio, numerator and denominator should be sigma for a gaussian. Otherwise, the heavier the tails, the higher from one. Then some norm coefficient (could be improved)
-        (q_plus2-q_plus1) / (q_plus1-median)/3.81 * 0.28067382 #alphaR 
-    ] 
+        (q_plus2-q_plus1) / (q_plus1-median)/3.81 * 0.28067382 #alphaR
+    ]
     try:
-        param_optimised,param_covariance_matrix = curve_fit(cruijff, h_forFit.axes[0].centers, h_forFit.values(), 
+        param_optimised,param_covariance_matrix = curve_fit(cruijff, h_forFit.axes[0].centers, h_forFit.values(),
             p0=p0, sigma=np.maximum(np.sqrt(h_forFit.values()), 1.8), absolute_sigma=True, maxfev=500000,
             bounds=np.transpose([(0., np.inf), (-np.inf, np.inf), (0., np.inf), (0., np.inf), (-np.inf, np.inf), (-np.inf, np.inf)])
             )
     except ValueError: # sometimes it fails with ValueError: array must not contain infs or NaNs and removing bounds helps
-        param_optimised,param_covariance_matrix = curve_fit(cruijff, h_forFit.axes[0].centers, h_forFit.values(), 
+        param_optimised,param_covariance_matrix = curve_fit(cruijff, h_forFit.axes[0].centers, h_forFit.values(),
             p0=p0, sigma=np.maximum(np.sqrt(h_forFit.values()), 1.8), absolute_sigma=True, maxfev=500000,
             #bounds=np.transpose([(0., np.inf), (-np.inf, np.inf), (0., np.inf), (0., np.inf), (-np.inf, np.inf), (-np.inf, np.inf)])
             )
@@ -92,34 +98,82 @@ def make_scOrTsOverCP_energy_histogram(name, label=None):
                   eta_axis, seedPt_axis, name=name, label=label)
     return h
 
-def fill_scOverCP_energy_histogram(h:hist.Hist, df:pd.DataFrame):
+def fill_scOverCP_energy_histogram(h:hist.Hist, df:pd.DataFrame, minEn:float, maxEn:float):
     """ df should be CPtoSC_df ie CaloParticle to Supercluster """
-    h.fill(e_ratio=df.raw_energy_supercls_sum/df.regressed_energy_CP,
-        absSeedEta=np.abs(df.barycenter_eta_seed),
-        seedPt=df.raw_pt_seed)
+    dfN = df.where((df.regressed_energy_CP >= minEn) & (df.regressed_energy_CP <= maxEn)).dropna()
+    h.fill(e_ratio=dfN.raw_energy/dfN.regressed_energy_CP)
 
 def fill_seedTsOverCP_energy_histogram(h:hist.Hist, df:pd.DataFrame):
     """ df should be CPtoTs_df ie CaloParticle to seed trackster (highest pt trackster for each endcap) """
     h.fill(e_ratio=df.raw_energy/df.regressed_energy_CP,
-        absSeedEta=np.abs(df.barycenter_eta),
-        seedPt=df.raw_pt)
+        seedPt=df.regressed_energy_CP)
+
+def make_num_eff_histo(name, bins, minVar, maxVar, label=None):
+    eratio_eff_axis = partial(hist.axis.Regular, bins, minVar, maxVar, name="e_ratio")
+    h = hist.Hist(eratio_eff_axis(label=label), name=name, label=label)
+    return h
+
+def fill_num_eff_histo(h:hist.Hist, df:pd.DataFrame, minShared:float, var:str, corrected:bool):
+    """ df should be CPtoSC_df ie CaloParticle to Supercluster """
+    if(corrected == False):
+        dfN = df.where((df.sharedE / df.raw_energy_CP >= minShared))
+    else:
+        dfN = df.where((df.sharedE / df.tot_sharedE >= minShared))
+    h.fill(getattr(dfN, var))
+
+def fill_den_eff_histo(h:hist.Hist, df:pd.DataFrame, minShared:float, var:str):
+    """ df should be CPtoSC_df ie CaloParticle to Supercluster """
+#     print(getattr(df,var))
+    h.fill(getattr(df, var))
 
 
-
-def fitMultiHistogram(h:hist.Hist) -> list[list[CruijffFitResult]]:
+def fitMultiHistogram(h:list[hist.Hist], sigmaEff:bool=False) -> list[list[CruijffFitResult]]:
     """ Cruijff fit of multi-dimensional histogram of Supercluster/CaloParticle energy """
     res = []
-    for eta_bin in range(len(h.axes["absSeedEta"])):
-        res.append([])
-        for seedPt_bin in range(len(h.axes["seedPt"])):
-            h_1d = h[{"absSeedEta":eta_bin, "seedPt":seedPt_bin}]
-            res[-1].append(fitCruijff(h_1d))
-    return res
+    sigmasEff = []
+#     for eta_bin in range(len(h[0].axes["absSeedEta"])):
 
-def etaBinToText(etaBin:int) -> str:
-    low, high = eta_axis[etaBin]
-    return r"$|\eta_{\text{seed}}| \in \left[" + f"{low}; {high}" + r"\right]$"
+    for i in range(len(h)):
+        h_1d = h[i]
+        res.append([])
+        sigmasEff.append([])
+#         for seedPt_bin in range(len(h)):
+#             print(eta_bin, seedPt_bin)
+#             h_1d = h[seedPt_bin][{"absSeedEta":eta_bin, "seedPt":0}]
+        fitR, sEff = fitCruijff(h_1d, sigmaEff)
+        res[-1] = fitR
+        sigmasEff[-1] = sEff
+    return (res, sigmasEff)
+
+def plotSingleHistWithFit(h_1d:hist.Hist, fitRes:CruijffFitResult, ax=None):
+    if ax is None:
+        _, ax = plt.subplots(figsize=(8, 8))
+    hep.histplot([h_1d], label=["Best associated trackster"], ax=ax, yerr=False, flow="none")
+    x_plotFct = np.linspace(h_1d.axes[0].centers[0], h_1d.axes[0].centers[-1], 500)
+    # print(fitRes)
+#     sigma = fitRes.params.sigmaEff if fitRes.params.sigmaEff != 0 else fitRes.params.sigmaAverage
+    # ax.plot(x_plotFct, cruijff(x_plotFct,*fitRes.params.makeTuple()), label=f"Cruijff fit\n$\sigma={fitRes.params.sigmaAverage:.3f}$")
+    # ax.set_xlim(0.1, 3.5)
+    # ax.set_ylabel("Events")
+    # ax.legend()
+    hep.cms.text("Preliminary", exp="TICLv5", ax=ax)
+    hep.cms.lumitext("PU=0", ax=ax)
+
+bin_edgesEnergy = [(9.5, 10.5), (19.5, 20.5), (29.5, 30.5), (49.5, 50.5), (69.5, 70.5), (99.5, 100.5), (199.5, 200.5), (399.5, 400.5), (599.5, 600.5)]
 
 def ptBinToText(ptBin:int) -> str:
-    low, high = seedPt_axis[ptBin]
-    return r"$E_{\text{T, seed}} \in \left[" + f"{low:.3g}; {high:.3g}" + r"\right]$"
+    low, high = bin_edgesEnergy[ptBin]
+    return r"$E_{\text{gen}} \in \left[" + f"{low:.3g}; {high:.3g}" + r"\right]$"
+
+def etaBinToTextEqual(etaBin:float) -> str:
+    return r"$|\eta_{\text{gen}}| =" + f"{etaBin}$"
+def plotAllFits(h:list[hist.Hist], fitResults:list[CruijffFitResult], etaFloat:float, bin_edgesEnergy, outputDir):
+
+    for i in range(len(h)):
+        seedPt_binT = bin_edgesEnergy[i]
+        h_1d = h[i]
+        eta_bins =  (1.89,1.91)
+        eta_bin = 0
+        plotSingleHistWithFit(h_1d, fitResults[0][i])
+        plt.text(0.05, 0.95, etaBinToTextEqual(etaFloat)+"\n"+ptBinToText(i), va="top", transform=plt.gca().transAxes, fontsize=20)
+        plt.savefig(f"{outputDir}/{seedPt_binT[0]}_{seedPt_binT[1]}_{eta_bin}.png")
